e86410eaada7bb7dbabd034e58c9eae60997a84a
f436y923 <70205098+f436y923@users.noreply.github.com>
2020-11-08 14:01:11 -0600

    Done queen class
    1. Attack function have been implemented
    2. move function have  been implemented

--- 448-Chess/queen.cpp
+++ 448-Chess/queen.cpp
@@ -1,7 +1,7 @@
 /* -----------------------------------------------------------------------------
  *
  * File Name:  queen.cpp
- * Author: Tristan Gant 
+ * Author: Tristan Gant
  * Assignment:   EECS-448 Project 3
  * Description:  cpp file for the queen class
  * Date: 10/22/2020
@@ -14,11 +14,11 @@
  queen::queen(int row, int col, int player)
  {
     currentRowPos = row;
-    currentColPos = col; 
+    currentColPos = col;
 
     if(player == 1) symbol = 'Q';
     else symbol = 'q';
- } 
+ }
 
  queen::~queen(){}
 
@@ -26,82 +26,75 @@
  {
     if(!validMove(row, col, b))
     {
-        throw(std::runtime_error("invalid move for QUEEN"));
+        throw(std::runtime_error("invalid move for Queen"));
     }
-    else 
+    else
     {
-        currentColPos = col; 
-        currentRowPos = row; 
+        currentColPos = col;
+        currentRowPos = row;
     }
  }
 
-void queen::attack(int row, int col, char **b)
+void queen::attack(int row, int col, char **b)//good
 {
-    if(!validMove(row, col, b))
+    if(!validAttack(row, col, b))
     {
-        throw(std::runtime_error("invalid attack for QUEEN"));
+      throw(std::runtime_error("invalid ATTACK for QUEEN"));
     }
-    else 
+    else
     {
-        currentColPos = col; 
-        currentRowPos = row; 
+      currentColPos = col;
+      currentRowPos = row;
     }
 }
 
-bool queen::validMove(int row, int col, char **b)
+bool queen::validAttack(int row, int col, char **b)
 {
-    return false;
-    //  if((row - currentRowPos == col - currentColPos || row - currentRowPos == currentColPos - col) && row - currentRowPos != 0)
-    // {
-    //     int rowpace = row -currentRowPos;
-    //     int colpace = col -currentColPos;
-    //     if (rowpace > 0)
-    //         rowpace = 1;
-    //     else
-    //         rowpace = -1;
-    //     if (colpace > 0)
-    //         colpace = 1;
-    //     else
-    //         colpace = -1;
-    //     int i = currentRowPos+rowpace;
-    //     int j = currentColPos+colpace;
-    //     for (; i!=row; i +=rowpace)
-    //     {
-    //         if (!emptySpace(b[i][j]))
-    //             return false;
-    //         else
-    //             j += colpace;
-    //     }
-    //     return true;
-    // }
-    // if (row == currentRowPos && col != currentColPos)
-    // {
-    //     int pace = col - currentColPos;
-    //     if (pace > 0)
-    //         pace = 1;
-    //     else
-    //         pace = -1;
-    //     for (int i =currentColPos + pace; i != col; i+=pace)
-    //         if (!emptySpace(b[row][i]))
-    //             return false;
-    //     return true;
-    // }
-    // else if (col == currentColPos && row != currentRowPos)
-    // {
-    //     int pace = row - currentRowPos;
-    //     if (pace > 0)
-    //         pace = 1;
-    //     else
-    //         pace = -1;
-    //     for (int i =currentRowPos + pace; i != row; i+=pace)
-    //         if (!emptySpace(b[i][col]))
-    //             return false;
-    //     return true;
-    // }
-    // else
-    //     return false;
+    if(!isOpposingPlayer(b[row][col])) return false;
+
+    else
+    {
+        if(currentRowPos == row && currentColPos == col) return false;
+
+        if(row == currentRowPos+1 && col == currentColPos+1) return true;
+
+        else if(row == currentRowPos+1 && col == currentColPos-1) return true;
+
+        else if(row == currentRowPos-1 && col == currentColPos+1) return true;
+
+        else if(row == currentRowPos-1 && col == currentColPos-1) return true;
+
+        else if(row == currentRowPos-1 && col == currentColPos) return true;
+
+        else if(row == currentRowPos+1 && col == currentColPos) return true;
+
+        else if(row == currentRowPos && col == currentColPos-1) return true;
+
+        else if(row == currentRowPos && col == currentColPos+1) return true;
+
+        else if(abs(row - currentRowPos) == abs(col - currentColPos)){ return true; }
+        else if(
+            (currentRowPos != row && currentColPos == col) ||
+            (currentColPos != col && currentRowPos == row)
+        ) return true;
+
+        else return false;
+    }
 }
 
+bool queen::validMove(int row, int col, char **b)//good
+{
+
+    if (!emptySpace(b[row][col])) return false;
+
+    else if(currentRowPos == row && currentColPos == col) return false;
+
+    else if(
+         abs(currentRowPos - row) != abs(currentColPos - col)  &&
+         currentRowPos != row && currentColPos != col
+       ) return false;
+    else return true;
+}
 
 char queen::getSymbol()const
 {
@@ -120,10 +113,10 @@ int queen::getCurrentColPos()const
 
 bool queen::emptySpace(char space)
 {
-  if(space == '-') 
-    return true; 
-  else 
-    return false; 
+  if(space == '-')
+    return true;
+  else
+    return false;
 }
 
 void queen::isDead()
@@ -133,23 +126,18 @@ void queen::isDead()
 
 bool queen::isOpposingPlayer(char s)
 {
-    if(symbol == 'Q') //if player1 
+    if(symbol == 'Q') //if player1
     {
-        if(s == 'p' || s == 'r' || s == 'n' || s == 'b' || s == 'q' || s == 'k') 
-            return true; 
-        else 
-            return false; 
+        if(s == 'p' || s == 'r' || s == 'n' || s == 'b' || s == 'q' || s == 'k')
+            return true;
+        else
+            return false;
     }
-    else 
+    else
     {
-        if(s == 'P' || s == 'R' || s == 'N' || s == 'B' || s == 'Q' || s == 'K') 
-            return true; 
-        else 
-            return false;       
+        if(s == 'P' || s == 'R' || s == 'N' || s == 'B' || s == 'Q' || s == 'K')
+            return true;
+        else
+            return false;
     }
-}
-
-bool queen::validAttack(int row, int col, char **b)
-{
-    return false; 
 }
